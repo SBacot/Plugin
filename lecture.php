7d32dcf64ef74200050d50e97b8019c0c0275d11
Sankot23 <s.bacot@gmail.com>
2023-06-02 16:03:29 +0200

    commit2

--- lecture.php
+++ lecture.php
@@ -36,32 +36,14 @@ add_filter( 'the_title', 'calcul_reading_time', 10, 2 );
 function restore_menu_item_title( $title, $item ) {
     // remove filter to get the original post title
     remove_filter( 'the_title', 'calcul_reading_time', 10, 2 );
+    remove_filter( 'the_title', 'calcul_reading_level', 10, 2 );
     // retrieve the original post title
     $title = get_the_title($item->object_id);
     // add the filter back
     add_filter( 'the_title', 'calcul_reading_time', 10, 2 );
+    add_filter( 'the_title', 'calcul_reading_level', 10, 2 );
     // return the title to display
     return $title;
-}
-
-
-function countSentences($sentences) {
-    $y = "";
-    $a = ".";
-    $b = "?";
-    $c = "!";
-    $numberOfSentences = 0;
-    $index = 0;
-
-    while($sentences != $y) {
-        $y .= $sentences[$index];
-        if ($sentences[$index] == $a || $sentences[$index] == $b || $sentences[$index] == $c) {
-            $numberOfSentences++;
-        }
-        $index++;
-    }
-    $numberOfSentences++;
-    return $numberOfSentences;
 }
 
 
@@ -69,59 +51,62 @@ function calcul_reading_level($title, $id = null) {
     if(!is_single()){
         return $title;
     }
-    $post = get_post();
+
+    $post = get_post($id);
     $content = strip_tags($post->post_content);
     // count the number of words in content
     $nb_letter = strlen($content);
-    $nb_words = str_word_count(strip_tags($content));
-    $sentence =countSentences($content);
+    $nb_words = max(1,str_word_count(strip_tags($content)));
+
+    // var_dump($nb_words);
+    $nbSentences  = count(preg_split('/[.|!|?]/', $content));
     $average_letters= (($nb_letter/$nb_words) * 100);
-    $average_words = (($sentence/$nb_words) * 100);
+    $average_words = (($nbSentences/$nb_words) * 100);
     $difficult= ceil((0.0588 * $average_letters) - (0.296 *  $average_words) - 15.8);
 
     //on fait le switch pour déterminer le niveau de 1 a 12 
+    $difficulty = "";
    switch ($difficult) {
-        case $difficult = '1':
-            $difficult = "CP";
+        case $difficult <= 1:
+            $difficulty = "CP";
             break;
-        case $difficult = '2':
-            $difficult = "CE1";
+        case $difficult == 2:
+            $difficulty = "CE1";
             break;
-        case $difficult = '3':
-            $difficult = "CE2";
+        case $difficult == 3:
+            $difficulty = "CE2";
             break;
-        case $difficult =  '4':
-            $difficult = "CM1";
+        case $difficult == 4:
+            $difficulty = "CM1";
             break;
-        case $difficult = '5':
-            $difficult = "CM2";
+        case $difficult == 5:
+            $difficulty = "CM2";
             break;
-        case $difficult = '6':
-            $difficult = "6em";
+        case $difficult == 6:
+            $difficulty = "6em";
             break;
-        case $difficult = '7':
-            $difficult = "5em";
+        case $difficult == 7:
+            $difficulty = "5em";
             break;
-        case $difficult = '8':
-            $difficult = "4em";
+        case $difficult == 8:
+            $difficulty = "4em";
             break;
-        case $difficult = '9':
-            $difficult = "3em";
+        case $difficult == 9:
+            $difficulty = "3em";
             break;
-        case $difficult = '10':
-            $difficult = "SECONDE";
+        case $difficult == 10:
+            $difficulty = "SECONDE";
             break;
-        case $difficult = '11':
-            $difficult = "PREMIERE";
+        case $difficult == 11:
+            $difficulty = "PREMIERE";
             break;
      default:
-            $difficult = "TERMINALE";
+            $difficulty = "TERMINALE";
             break;
     }
 
-
     // add the reading time in post title
-    $title .= "<small> | $difficult</small>";
+    $title .= "<small> | $difficulty</small>";
     return $title;
 
 }
